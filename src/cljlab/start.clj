142b8940251fcaa9ae5410b09f6846df5562e62a
puredanger <alex.miller@cognitect.com>
2015-02-16 10:52:24 -0600

    create TOC in start sheet

--- src/cljlab/start.clj
+++ src/cljlab/start.clj
@@ -6,6 +6,27 @@
 ;;; 
 ;; **
 
+;; **
+;;; ## How does this work?
+;;; 
+;;; TODO - explain Gorilla and how this works.
+;; **
+
+;; **
+;;; ## Table of Contents
+;;; 
+;;; * [Syntax](/worksheet.html?filename=src/cljlab/syntax.clj)
+;;; * [Evaluation](/worksheet.html?filename=src/cljlab/evaluation.clj)
+;;; * [Functions](/worksheet.html?filename=src/cljlab/functions.clj)
+;;; * [Collections](/worksheet.html?filename=src/cljlab/collections.clj)
+;;; * [Flow Control](/worksheet.html?filename=src/cljlab/flowcontrol.clj)
+;;; * [Namespaces](/worksheet.html?filename=src/cljlab/namespaces.clj)
+;;; * [Sequences](/worksheet.html?filename=src/cljlab/sequences.clj)
+;;; * [Polymorphism](/worksheet.html?filename=src/cljlab/polymorphism.clj)
+;;; * [State](/worksheet.html?filename=src/cljlab/state.clj)
+;;; 
+;; **
+
 ;; @@
-(ns cljlab.start)
+
 ;; @@
