5a348d6dbe6496ce64cfcacd8d1be0e63e31ceaa
puredanger <alex.miller@cognitect.com>
2015-04-10 15:21:28 -0500

    newest clojure

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-gorilla "0.3.5-SNAPSHOT"]]
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]])
