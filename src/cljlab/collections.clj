75c0aa6e74d42a77616feb088d4750d45b22f869
puredanger <alex.miller@cognitect.com>
2015-04-15 16:43:50 -0500

    collections done

--- src/cljlab/collections.clj
+++ src/cljlab/collections.clj
@@ -377,17 +377,390 @@ v
 ;; @@
 
 ;; **
-;;; You can get the same outcome with into:
+;;; There are a variety of other ways to build up a map using Clojure's sequence functions (which we have not yet discussed). Come back to these later!
 ;; **
 
 ;; @@
-(into {} (map #([% 0])))
+;; with map and into
+(into {} (map (fn [player] [player 0]) players))
+
+;; with reduce
+(reduce (fn [m player]
+          (assoc m player 0))
+        {} ; initial value
+        players)
 ;; @@
+
+;; **
+;;; ### Combining maps
+;;; 
+;;; The `merge` function can be used to combine multiple maps into a single map:
+;; **
+
+;; @@
+(def new-scores {"Cid" 300 "Baz" 900})
+(merge scores new-scores)
+
+;;=> {"Una" 1400, "Bob" 1240, "Baz" 900, "Cid" 300}
+;; @@
+
+;; **
+;;; We merged two maps here but you can pass more as well.
+;;; 
+;;; If both maps contain the same key, the rightmost one wins. Alternately, you can use `merge-with` to supply a function to invoke when there is a conflict:
+;; **
+
+;; @@
+(def new-scores {"Una" 550 "Cid" 900 "Samwise" 1000})
+(merge-with + scores new-scores)
+
+;;=> {"Samwise" 1000, "Una" 1950, "Bob" 1240, "Cid" 1924}
+;; @@
+
+;; **
+;;; In the case of a conflict, the function is called on both values to get the new value.
+;; **
+
+;; **
+;;; ### Sorted maps
+;;; 
+;;; Similar to sorted sets, sorted maps maintain the keys in sorted order based on a comparator, using `compare` as the default comparator function.
+;; **
+
+;; @@
+(def sm (sorted-map
+         "Bravo" 204
+         "Alfa" 35
+         "Sigma" 99
+         "Charlie" 100))
+
+;;=> {"Alfa" 35, "Bravo" 204, "Charlie" 100, "Sigma" 99}
+
+(keys sm)
+;;=> ("Alfa" "Bravo" "Charlie" "Sigma")
+
+(vals sm)
+;;=> (35 204 100 99)
+;; @@
+
+;; **
+;;; ## Representing entities
+;;; 
+;;; When we need to represent many information entities with the same set of fields known in advance, there are two common choices.
+;;; 
+;;; One common choice is to use a map with keyword keys.
+;; **
+
+;; @@
+(def person
+  {:first-name "Kelly"
+   :last-name "Keen"
+   :age 32
+   :occupation "Programmer"})
+;; @@
+
+;; **
+;;; ### Field accessors
+;;; 
+;;; Since this is a map, the ways we've already discussed for looking up a value by key work:
+;; **
 
 ;; **
 ;;; 
 ;; **
 
+;; @@
+(get person :occupation)
+(person :occupation)
+;; @@
+
+;; **
+;;; But really, the most common way to get field values for this use is by invoking the kewyord. Just like with maps and sets, keywords are also functions. When a keyword is invoked, it looks itself up in the associative data structure that it was passed.
+;; **
+
+;; @@
+(:occupation person)
+;;=> "Programmer"
+;; @@
+
+;; **
+;;; Keyword invocation also takes an optional default value:
+;; **
+
+;; @@
+(:favorite-color person "beige")
+;;=> "beige"
+;; @@
+
+;; **
+;;; ### Updating fields
+;;; 
+;;; Since this is a map, we can just use assoc to add or modify fields:
+;; **
+
+;; @@
+(assoc person :occupation "Baker")
+;;=> {:age 32, :last-name "Keen", :first-name "Kelly", :occupation "Baker"}
+;; @@
+
+;; **
+;;; ### Removing a field
+;;; 
+;;; Use dissoc to remove fields:
+;; **
+
+;; @@
+(dissoc person :age)
+;;=> {:last-name "Keen", :first-name "Kelly", :occupation "Programmer"}
+;; @@
+
+;; **
+;;; ### Nested entities
+;;; 
+;;; It is common to see entities nested within other entities:
+;; **
+
+;; @@
+(def company
+  {:name "WidgetCo"
+   :address {:street "123 Main St"
+             :city "Springfield"
+             :state "IL"}})
+;; @@
+
+;; **
+;;; We can use `get-in` to access fields at any level inside nested entities:
+;; **
+
+;; @@
+(get-in company [:address :city])
+;;=> "Springfield"
+;; @@
+
+;; **
+;;; We can also use `assoc-in` or `update-in` to modify nested entities:
+;; **
+
+;; @@
+(assoc-in company [:address :street] "303 Broadway")
+;; {:name "WidgetCo",
+;;  :address
+;;  {:state "IL",
+;;   :city "Springfield",
+;;   :street "303 Broadway"}}
+;; @@
+
+;; **
+;;; ### Records
+;;; 
+;;; An alternative to using maps is to create a "record". Records are designed specifically for this use case and generally have better performance. In addition, they have a named "type" which can be used for polymorphic behavior (more on that later).
+;;; 
+;;; Records are defined with the list of field names for record instances. These will be treated as keyword keys in each record instance.
+;; **
+
+;; @@
+;; Define a record structure
+(defrecord Person [first-name last-name age occupation])
+
+;; Positional constructor - generated
+(def kelly (->Person "Kelly" "Keen" 32 "Programmer"))
+
+;; Map constructor - generated
+(def kelly (map->Person
+             {:first-name "Kelly"
+              :last-name "Keen"
+              :age 32
+              :occupation "Programmer"}))
+;; @@
+
+;; **
+;;; Records are used almost exactly the same as maps, with the caveat that they cannot be invoked as a function like maps.
+;; **
+
+;; @@
+(:occupation kelly)
+;;=> "Programmer"
+;; @@
+
+;; **
+;;; # LAB: Collections
+;;; 
+;;; ## Building Collections 
+;;; 
+;;; ### Representing scores in a game
+;;; 
+;;; What collection would be best to represent a table of scores (represented by a number) for players (represented by a string) in a game?
+;; **
+
+;; **
+;;; ### Add a new player to the scores
+;;; 
+;;; Given a scores table:
+;; **
+
+;; @@
+(def scores {"Una" 1400, "Bob" 1240, "Cid" 1024})
+;; @@
+
+;; **
+;;; Write an expression that adds a new player "Mel" to the table.
+;; **
+
+;; @@
+
+;; @@
+
+;; **
+;;; ### Representing a player
+;;; 
+;;; How would you represent a player with the attributes `name` and `ranking`?
+;;; 
+;;; 
+;; **
+
+;; @@
+
+;; @@
+
+;; **
+;;; Given a player representation, what are some ways to retrieve the ranking given a player?
+;; **
+
+;; @@
+
+;; @@
+
+;; **
+;;; ## Modifying Nested Collections
+;;; 
+;;; ### Advance to the next round
+;;; 
+;;; A game's state is represented in a data structure like this:
+;; **
+
+;; @@
+(def game
+  {:round 2
+   :players #{{:name "Una" :ranking 43}
+              {:name "Bob" :ranking 77}
+              {:name "Cid" :ranking 33}}
+   :scores {"Una" 1400
+            "Bob" 1240
+            "Cid" 1024}})
+;; @@
+
+;; **
+;;; Write a function `next-round` that takes a game and returns it with the round incremented.
+;; **
+
+;; @@
+(defn next-round [game]
+  ___)
+
+;; Example use:
+
+(next-round game)
+;; => {:round 3
+;;     :players #{{:name "Una" :ranking 43}
+;;                {:name "Bob" :ranking 77}
+;;                {:name "Cid" :ranking 33}}
+;;     :scores {"Una" 1400
+;;              "Bob" 1240
+;;              "Cid" 1024}}
+;; @@
+
+;; **
+;;; ### Update the scores
+;;; 
+;;; Write a function `add-score` that increments a players score by a specified amount.
+;; **
+
+;; @@
+(defn add-score [game name score]
+  ___)
+
+;; Example use:
+
+(add-score game "Cid" 500)
+;; => {:round 3
+;;     :players #{{:name "Una" :ranking 43}
+;;                {:name "Bob" :ranking 77}
+;;                {:name "Cid" :ranking 33}}
+;;     :scores {"Una" 1400
+;;              "Bob" 1240
+;;              "Cid" 1524}}
+
+;; @@
+
+;; **
+;;; ### Add a new player
+;;; 
+;;; Write a function `add-player` that adds a new player and initializes their score.
+;; **
+
 ;; **
 ;;; 
 ;; **
+
+;; @@
+(defn add-player [game name]
+  ___)
+
+;; Example use:
+
+(add-player game "Mel" 24)
+;; => {:round 3
+;;     :players #{{:name "Una" :ranking 43}
+;;                {:name "Bob" :ranking 77}
+;;                {:name "Cid" :ranking 33}
+;;                {:name "Mel" :ranking 24}}
+;;     :scores {"Una" 1400
+;;              "Bob" 1240
+;;              "Cid" 1524
+;;              "Mel" 0}}
+;; @@
+
+;; **
+;;; ## Map Lookup
+;;; 
+;;; ### Looking up a player
+;;; 
+;;; We'd like to be able to look up a player's ranking based on their name. This is currently difficult. What data structure would make this easier?
+;; **
+
+;; @@
+
+;; @@
+
+;; **
+;;; ### Find a player
+;;; 
+;;; Using this game definition, how can we implement `find-player` for a game?
+;; **
+
+;; @@
+(def game
+  {:round 2
+   :players {"Una" {:name "Una" :ranking 43 :score 1400}
+             "Bob" {:name "Bob" :ranking 77 :score 1240}
+             "Cid" {:name "Cid" :ranking 33 :score 1024}}})
+
+(defn find-player [game name]
+  ___)
+
+;; Example:
+
+(find-player game "Una")
+;; => {:name "Una", :score 1400, :ranking 43}
+;; @@
+
+;; **
+;;; ### Remove a player
+;;; 
+;;; Write a function `remove-player` that removes a player from a game, including their score.
+;; **
+
+;; @@
+(defn remove-player [game name]
+  ___)
+;; @@
