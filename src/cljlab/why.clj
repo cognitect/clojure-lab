ae18dc81fddcf6b65691b2668df0f70af2b5e03c
puredanger <alex.miller@cognitect.com>
2015-04-16 10:27:27 -0500

    navigation, new why page

---
+++ src/cljlab/why.clj
@@ -0,0 +1,39 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Welcome to Clojure! 
+;;; 
+;;; 
+;; **
+
+;; **
+;;; ## How does this work?
+;;; 
+;;; You are currently reading a page hosted by [Gorilla REPL](http://gorilla-repl.org/). A "REPL" is the **Read-Eval-Print-Loop** - the beating heart inside any LISP. The Gorilla REPL creates interactive web pages that are a mix of text (in markdown) and code (live REPLs).
+;;; 
+;;; As you follow along in this course, you can evaluate any code block with Shift-Enter. For the full set of commands, type Ctrl-G Ctrl-G or use the drop-down in the upper right.
+;;; 
+;;; Because this page is editable, it will switch to edit mode when you select a text segment. This is somewhat annoying for this usage and an area we are working to improve, but for now, bear with it. 
+;;; 
+;;; Also note, to avoid editing a block with a link, right click and "open in new tab" for any links.
+;;; 
+;; **
+
+;; **
+;;; ## Table of Contents
+;;; 
+;;; * [Why Clojure?](/worksheet.html?filename=src/cljlab/why.clj)
+;;; * [Syntax](/worksheet.html?filename=src/cljlab/syntax.clj)
+;;; * [Functions](/worksheet.html?filename=src/cljlab/functions.clj)
+;;; * [Collections](/worksheet.html?filename=src/cljlab/collections.clj)
+;;; * [Flow Control](/worksheet.html?filename=src/cljlab/flowcontrol.clj)
+;;; * [Namespaces](/worksheet.html?filename=src/cljlab/namespaces.clj)
+;;; * [Sequences](/worksheet.html?filename=src/cljlab/sequences.clj)
+;;; * [Polymorphism](/worksheet.html?filename=src/cljlab/polymorphism.clj)
+;;; * [State](/worksheet.html?filename=src/cljlab/state.clj)
+;;; 
+;; **
+
+;; @@
+
+;; @@
