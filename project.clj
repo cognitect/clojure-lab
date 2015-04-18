2b711d0169a2e697672ee5ab03fbc9327f3f593e
puredanger <alex.miller@cognitect.com>
2015-04-17 21:29:35 -0500

    update project info

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
-(defproject clojure-workshop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojure-lab "0.1.0-SNAPSHOT"
+  :description "Interactive materials for learning Clojure"
+  :url "https://github.com/cognitect/clojure-lab"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-gorilla "0.3.5-SNAPSHOT"]]
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [gorilla-repl "0.3.5-SNAPSHOT"]]
+  :aot [start]
+  :main start)
