5509d1c110995f5011410d2b219c2f0e41662e2e
puredanger <alex.miller@cognitect.com>
2016-05-06 16:03:22 -0500

    update to latest gorilla repl

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-lab "0.1.0-SNAPSHOT"
   :description "Interactive materials for learning Clojure"
   :url "https://github.com/cognitect/clojure-lab"
-  :plugins [[lein-gorilla "0.3.5-SNAPSHOT"]]
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [gorilla-repl "0.3.5-SNAPSHOT"]]
+  :plugins [[lein-gorilla "0.3.6"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gorilla-repl "0.3.6"]]
   :aot [start]
   :main start)
